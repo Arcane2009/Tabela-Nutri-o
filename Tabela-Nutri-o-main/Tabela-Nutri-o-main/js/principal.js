f028749b54d4889e5f82af4bee5be5772bb046b9
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2025-08-28 12:37:53 -0300

    Add files via upload

--- Tabela-Nutri-o-main/Tabela-Nutri-o-main/js/principal.js
+++ Tabela-Nutri-o-main/Tabela-Nutri-o-main/js/principal.js
@@ -18,13 +18,26 @@ var altura = tdAltura.textContent;
 
 //Este bloco determina a variavel IMC do primeiro paciente e muda seu conteúdo//
 var IMC = peso / (altura * altura)
-var tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = IMC;
 
+//valores booleanos
+var pesoValido = true;
+var alturaValida = true;
 
+//alerta caso o peso e altura sejam invalidos
+if(peso <= 0 || peso >= 1000){
+    alert("Peso Inválido!");
+    var pesoValido = false;
+}
 
+if(altura <= 0 || altura >= 3.00){
+    alert("Altura Inválida!");
+    var alturaValida = false;
+}
 
-
+if(pesoValido && alturaValida){
+    var tdIMC = paciente.querySelector(".info-imc");
+    tdIMC.textContent = IMC;
+}
 
 
 
